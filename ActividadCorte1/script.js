94ad1e8cd1c72ff309003b130edbc63d4d83ca2a
danielgallegog2 <dgalle16@uniminuto.edu.co>
2026-03-29 20:33:33 -0500

    cambios

--- ActividadCorte1/script.js
+++ ActividadCorte1/script.js
@@ -1,4 +1,5 @@
-if ('serviceWorker' in navigator) {
+// file:// no es contexto seguro: el SW no aplica y fallaría; manifest también da CORS
+if ('serviceWorker' in navigator && window.isSecureContext) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
